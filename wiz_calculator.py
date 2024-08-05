aa130d1d82a450ffd72e457f3ce520d7818e488f
Yusef Wilson <yusefwilsonx@gmail.com>
2024-08-05 15:07:53 -0400

    Improve hanging effect buttons
    Consolidated traps and blades, added ability to add more than one copy of each hanging effect, changed internal hanging effect data structure for dict until calculation.

--- wiz_calculator.py
+++ wiz_calculator.py
@@ -30,73 +30,72 @@ enemy_resist.grid(row=1, column=3, padx=10, pady=5)
 effects_frame = tk.Frame(root)
 effects_frame.grid(row=1, column=0, padx=10, pady=10)
 
-# row of weakness buttons
-tk.Label(effects_frame, text="Weakness:", font=('Arial', 18)).grid(row=5, column=0, padx=10, pady=5, sticky='e')
-weakness_10 = tk.Button(effects_frame, text="10%", padx=10, pady=5, bg="red", command=lambda: add_weakness(weakness_10, 10))
-weakness_10.grid(row=5, column=1, padx=10, pady=5)
-weakness_15 = tk.Button(effects_frame, text="15%", padx=10, pady=5, bg="red", command=lambda: add_weakness(weakness_15, 15))
-weakness_15.grid(row=5, column=2, padx=10, pady=5)
-weakness_20 = tk.Button(effects_frame, text="20%", padx=10, pady=5, bg="red", command=lambda: add_weakness(weakness_20, 20))
-weakness_20.grid(row=5, column=3, padx=10, pady=5)
-weakness_25 = tk.Button(effects_frame, text="25%", padx=10, pady=5, bg="red", command=lambda: add_weakness(weakness_25, 25))
-weakness_25.grid(row=5, column=4, padx=10, pady=5)
-weakness_30 = tk.Button(effects_frame, text="30%", padx=10, pady=5, bg="red", command=lambda: add_weakness(weakness_30, 30))
-weakness_30.grid(row=5, column=5, padx=10, pady=5)
-weakness_35 = tk.Button(effects_frame, text="35%", padx=10, pady=5, bg="red", command=lambda: add_weakness(weakness_35, 35))
-weakness_35.grid(row=5, column=6, padx=10, pady=5)
-weakness_40 = tk.Button(effects_frame, text="40%", padx=10, pady=5, bg="red", command=lambda: add_weakness(weakness_40, 40))
-weakness_40.grid(row=5, column=7, padx=10, pady=5)
-weakness_45 = tk.Button(effects_frame, text="45%", padx=10, pady=5, bg="red", command=lambda: add_weakness(weakness_45, 45))
-weakness_45.grid(row=5, column=8, padx=10, pady=5)
-weakness_60 = tk.Button(effects_frame, text="60%", padx=10, pady=5, bg="red", command=lambda: add_weakness(weakness_60, 60))
-weakness_60.grid(row=5, column=9, padx=10, pady=5)
+def create_hanging_effect_widget(value: int, row: int, column: int, type: str) -> tuple[tk.Label, tk.Button, tk.Button]:
+    label = tk.Label(effects_frame, text=f"{value}% (0)", padx=5, pady=5, bg="orange")
+    label.grid(row=row, column=column, padx=10, pady=5)
+    plus = tk.Button(effects_frame, text="+", padx=0, pady=0, bg="green")
+    plus.grid(row=row, column=column + 1, padx=10, pady=5)
+    minus = tk.Button(effects_frame, text="-", padx=0, pady=0, bg="red")
+    minus.grid(row=row + 1, column=column + 1, padx=10, pady=5)
+    
+    if type == "buff":
+        plus.config(command=lambda: add_buff(label, value))
+        minus.config(command=lambda: remove_buff(label, value))
+    elif type == "shield":
+        plus.config(command=lambda: add_shield(label, value))
+        minus.config(command=lambda: remove_shield(label, value))
+    elif type == "weakness":
+        plus.config(command=lambda: add_weakness(label, value))
+        minus.config(command=lambda: remove_weakness(label, value))
 
-# row of blade buttons
-tk.Label(effects_frame, text="Blade:", font=('Arial', 18)).grid(row=6, column=0, padx=10, pady=5, sticky='e')
-blade_20 = tk.Button(effects_frame, text="20%", padx=10, pady=5, bg="red", command=lambda: add_buff(blade_20, 20))
-blade_20.grid(row=6, column=1, padx=10, pady=5)
-blade_25 = tk.Button(effects_frame, text="25%", padx=10, pady=5, bg="red", command=lambda: add_buff(blade_25, 25))
-blade_25.grid(row=6, column=2, padx=10, pady=5)
-blade_30 = tk.Button(effects_frame, text="30%", padx=10, pady=5, bg="red", command=lambda: add_buff(blade_30, 30))
-blade_30.grid(row=6, column=3, padx=10, pady=5)
-blade_35 = tk.Button(effects_frame, text="35%", padx=10, pady=5, bg="red", command=lambda: add_buff(blade_35, 35))
-blade_35.grid(row=6, column=4, padx=10, pady=5)
-blade_40 = tk.Button(effects_frame, text="40%", padx=10, pady=5, bg="red", command=lambda: add_buff(blade_40, 40))
-blade_40.grid(row=6, column=5, padx=10, pady=5)
-blade_55 = tk.Button(effects_frame, text="55%", padx=10, pady=5, bg="red", command=lambda: add_buff(blade_55, 55))
-blade_55.grid(row=6, column=6, padx=10, pady=5)
-blade_60 = tk.Button(effects_frame, text="60%", padx=10, pady=5, bg="red", command=lambda: add_buff(blade_60, 60))
-blade_60.grid(row=6, column=7, padx=10, pady=5)
+    return label, plus, minus
 
-# row of trap buttons
-tk.Label(effects_frame, text="Trap:", font=('Arial', 18)).grid(row=7, column=0, padx=10, pady=5, sticky='e')
-trap_20 = tk.Button(effects_frame, text="20%", padx=10, pady=5, bg="red", command=lambda: add_weakness(trap_20, 20))
-trap_20.grid(row=7, column=1, padx=10, pady=5)
-trap_25 = tk.Button(effects_frame, text="25%", padx=10, pady=5, bg="red", command=lambda: add_weakness(trap_25, 25))
-trap_25.grid(row=7, column=2, padx=10, pady=5)
-trap_30 = tk.Button(effects_frame, text="30%", padx=10, pady=5, bg="red", command=lambda: add_weakness(trap_30, 30))
-trap_30.grid(row=7, column=3, padx=10, pady=5)
-trap_35 = tk.Button(effects_frame, text="35%", padx=10, pady=5, bg="red", command=lambda: add_weakness(trap_35, 35))
-trap_35.grid(row=7, column=4, padx=10, pady=5)
-trap_70 = tk.Button(effects_frame, text="70%", padx=10, pady=5, bg="red", command=lambda: add_weakness(trap_70, 70))
-trap_70.grid(row=7, column=5, padx=10, pady=5)
+# row of weakness buttons (10,15,20,25,30,35,40,45,55,60,75)
+tk.Label(effects_frame, text="Weaknesses:", font=('Arial', 8)).grid(row=5, column=0, padx=10, pady=5, sticky='e')
+weakness_10_label, weakness_10_plus, weakness_10_minus = create_hanging_effect_widget(10, 5, 1, "weakness")
+weakness_15_label, weakness_15_plus, weakness_15_minus = create_hanging_effect_widget(15, 5, 3, "weakness")
+weakness_20_label, weakness_20_plus, weakness_20_minus = create_hanging_effect_widget(20, 5, 5, "weakness")
+weakness_25_label, weakness_25_plus, weakness_25_minus = create_hanging_effect_widget(25, 5, 7, "weakness")
+weakness_30_label, weakness_30_plus, weakness_30_minus = create_hanging_effect_widget(30, 5, 9, "weakness")
+weakness_35_label, weakness_35_plus, weakness_35_minus = create_hanging_effect_widget(35, 5, 11, "weakness")
+weakness_40_label, weakness_40_plus, weakness_40_minus = create_hanging_effect_widget(40, 5, 13, "weakness")
+weakness_45_label, weakness_45_plus, weakness_45_minus = create_hanging_effect_widget(45, 5, 15, "weakness")
+weakness_55_label, weakness_55_plus, weakness_55_minus = create_hanging_effect_widget(55, 5, 17, "weakness")
+weakness_60_label, weakness_60_plus, weakness_60_minus = create_hanging_effect_widget(60, 5, 19, "weakness")
+weakness_75_label, weakness_75_plus, weakness_75_minus = create_hanging_effect_widget(75, 5, 21, "weakness")
+
+# row of buff buttons
+tk.Label(effects_frame, text="Buffs \n(Blades, Traps, Bubbles, Auras):", font=('Arial', 8)).grid(row=7, column=0, padx=10, pady=5, sticky='e')
+buff_10_label, buff_10_plus, buff_10_minus = create_hanging_effect_widget(10, 7, 1, "buff")
+buff_15_label, buff_15_plus, buff_15_minus = create_hanging_effect_widget(15, 7, 3, "buff")
+buff_20_label, buff_20_plus, buff_20_minus = create_hanging_effect_widget(20, 7, 5, "buff")
+buff_25_label, buff_25_plus, buff_25_minus = create_hanging_effect_widget(25, 7, 7, "buff")
+buff_30_label, buff_30_plus, buff_30_minus = create_hanging_effect_widget(30, 7, 9, "buff")
+buff_35_label, buff_35_plus, buff_35_minus = create_hanging_effect_widget(35, 7, 11, "buff")
+buff_40_label, buff_40_plus, buff_40_minus = create_hanging_effect_widget(40, 7, 13, "buff")
+buff_45_label, buff_45_plus, buff_45_minus = create_hanging_effect_widget(45, 7, 15, "buff")
+buff_50_label, buff_50_plus, buff_50_minus = create_hanging_effect_widget(50, 7, 17, "buff")
+buff_55_label, buff_55_plus, buff_55_minus = create_hanging_effect_widget(55, 7, 19, "buff")
+buff_60_label, buff_60_plus, buff_60_minus = create_hanging_effect_widget(60, 7, 21, "buff")
+buff_70_label, buff_70_plus, buff_70_minus = create_hanging_effect_widget(70, 7, 23, "buff")
+buff_80_label, buff_80_plus, buff_80_minus = create_hanging_effect_widget(80, 7, 25, "buff")
 
 # row of shield buttons
-tk.Label(effects_frame, text="Shield:", font=('Arial', 18)).grid(row=8, column=0, padx=10, pady=5, sticky='e')
-shield_20 = tk.Button(effects_frame, text="20%", padx=10, pady=5, bg="red", command=lambda: add_shield(shield_20, 20))
-shield_20.grid(row=8, column=1, padx=10, pady=5)
-shield_25 = tk.Button(effects_frame, text="25%", padx=10, pady=5, bg="red", command=lambda: add_shield(shield_25, 25))
-shield_25.grid(row=8, column=2, padx=10, pady=5)
-shield_40 = tk.Button(effects_frame, text="40%", padx=10, pady=5, bg="red", command=lambda: add_shield(shield_40, 40))
-shield_40.grid(row=8, column=3, padx=10, pady=5)
-shield_50 = tk.Button(effects_frame, text="50%", padx=10, pady=5, bg="red", command=lambda: add_shield(shield_50, 50)) 
-shield_50.grid(row=8, column=4, padx=10, pady=5)
-shield_70 = tk.Button(effects_frame, text="70%", padx=10, pady=5, bg="red", command=lambda: add_shield(shield_70, 70))
-shield_70.grid(row=8, column=5, padx=10, pady=5)
-shield_80 = tk.Button(effects_frame, text="80%", padx=10, pady=5, bg="red", command=lambda: add_shield(shield_80, 80))
-shield_80.grid(row=8, column=6, padx=10, pady=5)
-shield_85 = tk.Button(effects_frame, text="85%", padx=10, pady=5, bg="red", command=lambda: add_shield(shield_85, 85))
-shield_85.grid(row=8, column=7, padx=10, pady=5)
+tk.Label(effects_frame, text="Shields \n (includes defensive auras):", font=('Arial', 8)).grid(row=9, column=0, padx=10, pady=5, sticky='e')
+shield_10_label, shield_10_plus, shield_10_minus = create_hanging_effect_widget(10, 9, 1, "shield")
+shield_15_label, shield_15_plus, shield_15_minus = create_hanging_effect_widget(15, 9, 3, "shield")
+shield_20_label, shield_20_plus, shield_20_minus = create_hanging_effect_widget(20, 9, 5, "shield")
+shield_25_label, shield_25_plus, shield_25_minus = create_hanging_effect_widget(25, 9, 7, "shield")
+shield_30_label, shield_30_plus, shield_30_minus = create_hanging_effect_widget(30, 9, 9, "shield")
+shield_35_label, shield_35_plus, shield_35_minus = create_hanging_effect_widget(35, 9, 11, "shield")
+shield_40_label, shield_40_plus, shield_40_minus = create_hanging_effect_widget(40, 9, 13, "shield")
+shield_45_label, shield_45_plus, shield_45_minus = create_hanging_effect_widget(45, 9, 15, "shield")
+shield_50_label, shield_50_plus, shield_50_minus = create_hanging_effect_widget(50, 9, 17, "shield")
+shield_55_label, shield_55_plus, shield_55_minus = create_hanging_effect_widget(55, 9, 19, "shield")
+shield_60_label, shield_60_plus, shield_60_minus = create_hanging_effect_widget(60, 9, 21, "shield")
+shield_70_label, shield_70_plus, shield_70_minus = create_hanging_effect_widget(70, 9, 23, "shield")
+shield_80_label, shield_80_plus, shield_80_minus = create_hanging_effect_widget(80, 9, 25, "shield")
+shield_85_label, shield_85_plus, shield_85_minus = create_hanging_effect_widget(85, 9, 27, "shield")
 
 # frame for base spell damage
 spell_frame = tk.Frame(root)
@@ -133,9 +132,9 @@ button_calculation_type.grid(row=1, column=2, padx=10, pady=5)
 calculating_damage = True
 
 # state variables for effects
-buffs = []
-shields = []
-weaknesses = []
+buffs = {}
+shields = {}
+weaknesses = {}
 
 def toggle_button_color(button):
     if button.cget("bg") == "green":
@@ -144,29 +143,53 @@ def toggle_button_color(button):
         button.config(bg="green")
 
 # button functions for shields and traps
-def add_buff(button, buff):
-    toggle_button_color(button)
+def add_buff(button, buff: int):
     # add the buff to the list of buffs if button is being checked. Otherwise, remove it.
     if buff in buffs:
-        buffs.remove(buff)
+        buffs[buff] += 1
     else:
-        buffs.append(buff)
+        buffs[buff] = 1
+    # change button text to reflect the number of buffs
+    button.config(text=f"{buff}% ({buffs[buff]})")
 
-def add_shield(button, shield):
-    toggle_button_color(button)
+def remove_buff(button, buff: int):
+    # remove the buff from the list of buffs
+    if buff in buffs:
+        buffs[buff] = max(buffs[buff] - 1, 0)
+    # change button text to reflect the number of buffs
+    button.config(text=f"{buff}% ({buffs[buff]})")
+
+def add_shield(button, shield: int):
     # add the shield to the list of shields if button is being checked. Otherwise, remove it.
     if shield in shields:
-        shields.remove(shield)
+        shields[shield] += 1
     else:
-        shields.append(shield)
+        shields[shield] = 1
+    # change button text to reflect the number of shields
+    button.config(text=f"{shield}% ({shields[shield]})")
 
-def add_weakness(button, weakness):
-    toggle_button_color(button)
+def remove_shield(button, shield: int):
+    # remove the shield from the list of shields
+    if shield in shields:
+        shields[shield] = max(shields[shield] - 1, 0)
+    # change button text to reflect the number of shields
+    button.config(text=f"{shield}% ({shields[shield]})")
+
+def add_weakness(button, weakness: int):
     # add the weakness to the list of weaknesses if button is being checked. Otherwise, remove it.
     if weakness in weaknesses:
-        weaknesses.remove(weakness)
+        weaknesses[weakness] += 1
     else:
-        weaknesses.append(weakness)
+        weaknesses[weakness] = 1
+    # change button text to reflect the number of weaknesses
+    button.config(text=f"{weakness}% ({weaknesses[weakness]})")
+
+def remove_weakness(button, weakness: int):
+    # remove the weakness from the list of weaknesses
+    if weakness in weaknesses:
+        weaknesses[weakness] = max(weaknesses[weakness] - 1, 0)
+    # change button text to reflect the number of weaknesses
+    button.config(text=f"{weakness}% ({weaknesses[weakness]})")
 
 def toggle_calculation_type(hitter_button):
 
@@ -182,18 +205,18 @@ def toggle_calculation_type(hitter_button):
 def clear():
 
     global buffs, shields, weaknesses
-    buffs = []
-    shields = []
-    weaknesses = []
 
-    for button in [weakness_10, weakness_15, weakness_20, weakness_25, weakness_30, weakness_35, weakness_40, weakness_45, weakness_60]:
-        button.config(bg="red")
-    for button in [blade_20, blade_25, blade_30, blade_35, blade_40, blade_55, blade_60]:
-        button.config(bg="red")
-    for button in [trap_20, trap_25, trap_30, trap_35, trap_70]:
-        button.config(bg="red")
-    for button in [shield_20, shield_25, shield_40, shield_50, shield_70, shield_80, shield_85]:
-        button.config(bg="red")
+    buffs = {}
+    shields = {}
+    weaknesses = {}
+
+    # reset text of all button labels
+    for button_label in [weakness_10_label, weakness_15_label, weakness_20_label, weakness_25_label, weakness_30_label, weakness_35_label, weakness_40_label, weakness_45_label, weakness_55_label, weakness_60_label, weakness_75_label]:
+        button_label.config(text=button_label.cget("text").split("(")[0] + "(0)")
+    for button_label in [buff_10_label, buff_15_label, buff_20_label, buff_25_label, buff_30_label, buff_35_label, buff_40_label, buff_45_label, buff_50_label, buff_55_label, buff_60_label, buff_70_label, buff_80_label]:
+        button_label.config(text=button_label.cget("text").split("(")[0] + "(0)")
+    for button_label in [shield_10_label, shield_15_label, shield_20_label, shield_25_label, shield_30_label, shield_35_label, shield_40_label, shield_45_label, shield_50_label, shield_55_label, shield_60_label, shield_70_label, shield_80_label, shield_85_label]:
+        button_label.config(text=button_label.cget("text").split("(")[0] + "(0)")
 
     player_damage.delete(0, tk.END)
     player_pierce.delete(0, tk.END)
@@ -231,6 +254,8 @@ def damage_after_buffs(damage: float, buffs: list[float]) -> float:
         total_buff *= (1 + buff / 100)
     return damage * total_buff
 
+# final calculations
+
 # ALL HANGING EFFECTS ARE PASSED AS THEIR INTEGER VALUES (e.g. 20% is passed as 20).
 # RETURNS: A tuple containing the final damage and the final critical damage respectively.
 def damage_calculation(spell_damage: int, buffs: list[float], pierce: float, shields: list[float], weaknesses: list[float], crit_multiplier: float, enemy_resist: float) -> tuple[float]:
@@ -267,6 +292,13 @@ def resist_calculation(spell_damage: int, buffs: list[float], pierce: float, shi
 
     return effective_resist, maximum_potential_resist
 
+def dict_to_list(d: dict) -> list:
+    l = []
+    for key in d:
+        for i in range(d[key]):
+            l.append(key)
+    return l
+
 def calculate():
     new_hitter_damage, new_pierce, new_crit_multiplier, new_enemy_resist = 0, 0, 1, 0
 
@@ -277,9 +309,14 @@ def calculate():
 
     new_spell_damage = float(spell_damage.get()) if spell_damage.get() else 0
     new_actual_damage = float(actual_damage.get()) if actual_damage.get() else 0
-    new_buffs = buffs + [new_hitter_damage] if new_hitter_damage > 0 else buffs
-    new_shields = shields
-    new_weaknesses = weaknesses
+    
+    # add player damage to list of buffs
+    new_buffs = buffs
+    new_buffs[new_hitter_damage] = 1
+    new_buffs = dict_to_list(new_buffs)
+
+    new_shields = dict_to_list(shields)
+    new_weaknesses = dict_to_list(weaknesses)
     
     print('buffs:', new_buffs, 'shields:', new_shields, 'weaknesses:', new_weaknesses, 'pierce:', new_pierce, 'crit_multiplier:', new_crit_multiplier, 'enemy_resist:', new_enemy_resist, 'spell_damage:', new_spell_damage)
     
